fed06bbcdec5acfb4d4974870f2581f1fca544c2
Shawn Ren <doggy-ren@hotmail.com>
2019-03-23 19:42:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, items|
     items.each do |feature, arr|
       arr.each do |name|
-        names << name if !names.include?(name)
+        names << name if names.include?(name)
       end
     end
   end
